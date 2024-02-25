a01965822c688fa4ed743c89aab14396c1dddecd
kanto-taniguchi <kanto.jobs@gmail.com>
2024-02-26 01:28:48 +0900

    UPDATE

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 
-//START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+//START_ASYNC(非同期) -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    // 新しい入力がハッシュと一致するかどうかを確認する
+    bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {  // 第一引数はハッシュ化されていないデータ
+        console.log(res);
+    });
+});
 //END_ASYNC
 
 //START_SYNC
